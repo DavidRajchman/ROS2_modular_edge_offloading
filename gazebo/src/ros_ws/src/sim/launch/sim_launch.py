a2f5585171c4e35252900a2b9e0a782773dbe8cf
Adam Janes <janesada@fel.cvut.cz>
2025-02-13 13:18:56 +0100

    bridges cmd, odom, cam, lidar, imu, testeed in RVIZ

--- gazebo/src/ros_ws/src/sim/launch/sim_launch.py
+++ gazebo/src/ros_ws/src/sim/launch/sim_launch.py
@@ -35,15 +35,84 @@ def generate_launch_description():
                 namespace='ros_gz_bridge',
                 executable='parameter_bridge',
                 name='gz_bridge_clock',
-                arguments=[ '/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock']
+                arguments=[ '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock']
+            )
+        ]
+    )
+
+    gazebo_bridge_lidar = TimerAction(
+        period=5.0,  # Zpoždění v sekundách
+        actions=[
+            Node(
+                package='ros_gz_bridge',
+                namespace='ros_gz_bridge',
+                executable='parameter_bridge',
+                name='gz_bridge_lidar',
+                arguments=[ '/lidar@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan']
+            )
+        ]
+    )
+
+    gazebo_bridge_cam = TimerAction(
+        period=5.0,  # Zpoždění v sekundách
+        actions=[
+            Node(
+                package='ros_gz_bridge',
+                namespace='ros_gz_bridge',
+                executable='parameter_bridge',
+                name='gz_bridge_cam',
+                arguments=[ '/camera/image_raw@sensor_msgs/msg/Image[gz.msgs.Image']
+            )
+        ]
+    )
+    
+    gazebo_bridge_imu = TimerAction(
+        period=5.0,  # Zpoždění v sekundách
+        actions=[
+            Node(
+                package='ros_gz_bridge',
+                namespace='ros_gz_bridge',
+                executable='parameter_bridge',
+                name='gz_bridge_imu',
+                arguments=[ '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU']
+            )
+        ]
+    )
+
+    gazebo_bridge_odom = TimerAction(
+        period=5.0,  # Zpoždění v sekundách
+        actions=[
+            Node(
+                package='ros_gz_bridge',
+                namespace='ros_gz_bridge',
+                executable='parameter_bridge',
+                name='gz_bridge_odom',
+                arguments=[ '/model/vehicle_blue/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry']
+            )
+        ]
+    )
+
+    gazebo_bridge_cmd = TimerAction(
+        period=5.0,  # Zpoždění v sekundách
+        actions=[
+            Node(
+                package='ros_gz_bridge',
+                namespace='ros_gz_bridge',
+                executable='parameter_bridge',
+                name='gz_bridge_cmd',
+                arguments=[ '/model/vehicle_blue/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist']
             )
         ]
     )
 
     return LaunchDescription([
         gazebo_sim,
-        gazebo_bridge_clock
-        # gazebo_bridge
+        gazebo_bridge_clock,
+        gazebo_bridge_lidar,
+        gazebo_bridge_cam,
+        gazebo_bridge_imu,
+        gazebo_bridge_odom,
+        gazebo_bridge_cmd
 
         
     ])
