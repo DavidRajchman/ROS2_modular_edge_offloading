d8f59c02cea19c08fc25fd58c5a9d3bcc5259548
Adam Janes <janesada@fel.cvut.cz>
2025-02-13 11:45:41 +0100

    running simulation with our vehicle model, spawning gazebo bridge (now for clock topic)

--- gazebo/src/ros_ws/src/sim/launch/sim_launch.py
+++ gazebo/src/ros_ws/src/sim/launch/sim_launch.py
@@ -6,6 +6,7 @@ from launch_ros.actions import Node
 from launch_ros.descriptions import Parameter
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import TimerAction
 
 
 
@@ -15,21 +16,34 @@ def generate_launch_description():
     # Zahrnutí jiného launch souboru
     # other_launch_file = LaunchConfiguration('ros_gz_sim', default='gz_sim.launch.py')
     ros_gz_sim_launch_file = get_package_share_directory('ros_gz_sim') + '/launch/gz_sim.launch.py'
-
-    return LaunchDescription([
-        # Zahrnutí jiného launch souboru
-        launch.actions.IncludeLaunchDescription(
+    gazebo_sim = launch.actions.IncludeLaunchDescription(
             # PythonLaunchDescriptionSource([LaunchConfiguration('ros_gz_sim'), '/', other_launch_file])
-            PythonLaunchDescriptionSource(ros_gz_sim_launch_file)
-        ),
-        # Další akce pro tento soubor
-        LogInfo(msg="Main launch file is running 3 ."),
-        
-        Node(
+            PythonLaunchDescriptionSource(ros_gz_sim_launch_file),
+            launch_arguments={'gz_args': '/home/ubuntu/ros_ws/src/sim/description/robot.sdf'}.items()
+        )
+    gazebo_bridge = Node(
             package='demo_nodes_cpp',
             namespace='demo_nodes_cpp',
             executable='talker',
             name='sim_talker'
         )
+    gazebo_bridge_clock = TimerAction(
+        period=5.0,  # Zpoždění v sekundách
+        actions=[
+            Node(
+                package='ros_gz_bridge',
+                namespace='ros_gz_bridge',
+                executable='parameter_bridge',
+                name='gz_bridge_clock',
+                arguments=[ '/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock']
+            )
+        ]
+    )
+
+    return LaunchDescription([
+        gazebo_sim,
+        gazebo_bridge_clock
+        # gazebo_bridge
+
         
     ])
